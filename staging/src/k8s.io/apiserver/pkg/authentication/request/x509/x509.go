fe7137592d8daa2b9d474cda28a9a012fd9f3370
Luke Swart <hi@modulitos.com>
2024-10-07 17:20:37 -0700

    Update staging/src/k8s.io/apiserver/pkg/authentication/request/x509/x509.go
    Co-authored-by: Mo Khan <i@monis.app>

--- staging/src/k8s.io/apiserver/pkg/authentication/request/x509/x509.go
+++ staging/src/k8s.io/apiserver/pkg/authentication/request/x509/x509.go
@@ -303,7 +303,7 @@ var uidOID = asn1.ObjectIdentifier([]int{0, 9, 2342, 19200300, 100, 1, 1})
 func parseUIDFromCert(cert *x509.Certificate) string {
 	for _, name := range cert.Subject.Names {
 		if name.Type.Equal(uidOID) {
-			if s, ok := name.Value.(string); ok {
+			if s, ok := name.Value.(string); ok && len(s) > 0 {
 				return s
 			}
 		}
